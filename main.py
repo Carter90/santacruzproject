1297c89008b72a6a4c2979b4a120488fba9bcb81
Carter Frost <carter90@gmail.com>
2020-03-25 17:55:02 -0700

    main.py: now using data from downtownsantacruz's API and PEP8 compliance

--- main.py
+++ main.py
@@ -1,35 +1,42 @@
 #!/usr/bin/env python3.6
-''' 
+"""
 Carter Frost
-For hosting a website for Santa Cruz Map for businesses a virtual downtown shopping experience, making it easier for locals to support local businesses. 
-'''
+For hosting a website for Santa Cruz Map for businesses a virtual downtown shopping experience,
+making it easier for locals to support local businesses.
+"""
 
-import logging
-
-from flask import Flask, render_template, request
+import requests
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'])
 def main():
-    businesses = list()
-    with open('data/Eat.txt', 'r') as file:
-            businesses = [(bID, *business.split(',')) for bID, business in enumerate(file.read().split('\n'), 1)]
-    businesses.pop() # last one is not a real business #TODO double check after everything is working
-    return render_template('index.html',businesses=businesses)
+	businesses_data = requests.get(url="https://downtownsantacruz.com/_api/v2/points.json").json()
+	businesses = [(business['properties']['point_id'],
+	               business['geometry']['coordinates'][0],
+	               business['geometry']['coordinates'][1],
+	               business['properties']['point_name'],
+	               business['properties']['address1'],
+	               "/go/" + business['properties']['point_alias']
+	               ) for business in businesses_data]
+	return render_template('index.html', businesses=businesses)
+
 
 @app.route('/static/<name>')
-def resource(name) :
-    '''Load a file from the static directory and return it to the browser.''' 
-    with open ('static/' + name, 'rb') as f :
-        return f.read()
+def resource(name):
+	"""Load a file from the static directory and return it to the browser."""
+	with open('static/' + name, 'rb') as f:
+		return f.read()
+
 
 @app.errorhandler(500)
 def server_error(e):
-    ''' Just that it is an error handler'''
-    print("Borked",e)
-    return('I done borked up.', 500)
-    
-if __name__ == '__main__' : 
-    app.run(host='0.0.0.0', port=8080)
+	""" Just that it is an error handler"""
+	print("Borked", e)
+	return 'I done borked up.', 500
 
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=8080)
