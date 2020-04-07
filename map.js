819fd90a2e2ab927e9846bcc2e360bb4f70ae74a
Carter Frost <Carter90@gmail.com>
2020-04-07 13:50:18 -0700

    map.js: update Google API key to mine, prior commit key got chopped off

--- map.js
+++ map.js
@@ -74,7 +74,7 @@ const goInside = () => {
 
 // Gets Coordinates of road from nearest lat, lng using roads api
 const getRoadCoords = async (lat, lng) => {
-  return await fetch('https://roads.googleapis.com/v1/nearestRoads?points=' + lat + ',' + lng + '&key=AIzaSyDVjNL6rNtWlbUFymUbRb3KFRvMDTkIC')
+  return await fetch('https://roads.googleapis.com/v1/nearestRoads?points=' + lat + ',' + lng + '&key=AIzaSyDVjNL6rNtWlbUFymUbRb3KFRvMDTkIC-k')
   .then((response) => {
     return response.json()
   })
